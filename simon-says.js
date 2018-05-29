205b054d2f03bb4705a87cde3e2694c5524356ed
Austin Gorman <austin.gorman@gmail.com>
2018-05-29 12:28:36 -0500

    fixed scope issues

--- simon-says.js
+++ simon-says.js
@@ -1,12 +1,15 @@
 const locations = [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3]]
+let invalidLocation = false
+let k = 0;
 
-for (let k = 0; k < locations.length; k++) {
-    const currentLocation = locations[k]
-
+for (k = 0; k < locations.length; k++) {
+    let currentLocation = locations[k]
+        
     if (currentLocation[0] > 2) {
-        const invalidLocation = true
-
+        invalidLocation = true
+    } 
+    if (invalidLocation) {
         console.log("This location is invalid")
     }
-    console.log(`There were ${k} locations displayed`)
 }
+console.log(`There were ${k} locations displayed`)
